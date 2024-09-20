c35e02d8c03d86a9a1548d5efb8eee560a9d583c
Konstantinos Voulgaridis <kos.voulgaridis@gmail.com>
2024-09-20 15:36:42 +0300

    Mjut (#2)
    * mjut - mocking modules
    
    * Restructure folders
    
    * Interaction testing while mocking
    
    * More interaction testing
    
    * Partial mocking
    
    * Spying on functions
    
    * Options to clear mocks after using them
    
    * Mocking dates
    
    * Mocking more dates
    
    * Install prettier and format all files
    
    * Create a command to format files
    
    * Install eslint, fix the detected issues and create a lint custom command
    
    * Update readme
    
    * Make eslint ignore dist files and typescript node_module files
    
    * Custom command to run tsc
    
    * Fix ignores pattern on eslint config

--- mastering-javascript-unit-testing/src/lib/getShippingQuote.ts
+++ mastering-javascript-unit-testing/src/lib/getShippingQuote.ts
@@ -1,4 +1,5 @@
 export const getShippingQuote = (destination: string) => {
+  if (!destination) return "";
   console.log(`Getting a shipping quote for ${destination}...`);
   return { cost: 10 * Math.random(), estimatedDays: 2 };
 };
---
+++ mastering-javascript-unit-testing/src/lib/security.ts
@@ -0,0 +1,5 @@
+export function generateCode() {
+  return Math.floor(Math.random() * (999999 - 100000 + 1)) + 100000;
+}
+
+export default { generateCode };
